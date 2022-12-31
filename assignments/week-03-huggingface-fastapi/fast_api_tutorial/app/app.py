4e4aa15689b4d2e2c63cbb8abb143907b8804ce7
kimberly <kimberly.lantay.wright@gmail.com>
2022-12-30 21:02:41 -0500

    fixing the mess

---
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+    ccdcd
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+ 
