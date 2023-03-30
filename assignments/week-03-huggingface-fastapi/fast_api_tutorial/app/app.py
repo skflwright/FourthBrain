05f049b110c44b39c97b5405296d8c68f2bd651c
skfl <kimberly.lantay.wright@gmail.com>
2023-03-29 21:41:40 -0400

    fixed app

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
-#from fastapi import FastAPI, Request, Response
 from pydantic import BaseModel
+from typing import List
+import transformers
 
-#import transformers
+# Load the saved model
 
-#load the saved model
-translator = "fast_api_tutorial/app/model/my_awesome_model"
+translator = transformers.pipeline("translation_en_to_de","model/my_awesome_model")
 pipeline = translator
-
 app = FastAPI()
 
 class TextToTranslate(BaseModel):
     input_text: str
 
+class ListToTranslate(BaseModel):
+    input_texts: List[str]
+    
+    
 @app.get("/")
 def index():
     return {"message": "Hello World"}
@@ -30,9 +33,54 @@ def translate(input_text: TextToTranslate):
     return translator(input_text.input_text)
 
 @app.post("/batch_translation")
-def batch_translation(request: TextToTranslate):
-    translate_text = request.json()
-    translated_text = translator(translate_text['input_text'])
+def batch_translation(input_text: ListToTranslate):
+    print(input_text.input_texts)
+    translated_text = translator(input_text.input_texts)
+    return translated_text
+  
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+
+"""
+from fastapi import FastAPI
+#from fastapi import FastAPI, Request, Response
+from pydantic import BaseModel
+from typing import List
+#import transformers
+
+#load the saved model
+translator = "fast_api_tutorial/app/model/my_awesome_model"
+pipeline = translator
+
+app = FastAPI()
+
+class TextToTranslate(BaseModel):
+    input_text: str
+
+class ListToTranslate(BaseModel):
+    input_texts: List()
+    
+    
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+ 
+@app.post("/echo")
+def echo(text_to_translate: TextToTranslate):
+    return {"message": text_to_translate.input_text}
+
+@app.post("/translate")
+def translate(input_text: TextToTranslate):
+    return translator(input_text.input_text)
+
+@app.post("/batch_translation")
+def batch_translation(request: ListToTranslate):
+    translated_text = translator(request.input_texts)
     return translator(translated_text[0])
   
 if __name__ == "__main__":
@@ -41,7 +89,7 @@ if __name__ == "__main__":
 
 
 
-"""
+
 import transformers
 from transformers import pipeline
 from fastapi import FastAPI, Request, Response
